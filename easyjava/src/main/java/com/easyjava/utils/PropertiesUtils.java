359df54156273fe80c8ce44bfa3fdad7d565055a
q2w2h2 <1349355096@qq.com>
2023-05-07 20:52:54 +0800

    初始配置

---
+++ easyjava/src/main/java/com/easyjava/utils/PropertiesUtils.java
@@ -0,0 +1,47 @@
+package com.easyjava.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class PropertiesUtils {
+    private static Properties props = new Properties();
+    private static Map<String,String> PROPER_MAP = new ConcurrentHashMap<String, String>();
+
+    static {
+        InputStream in = null;
+        try {
+            in = PropertiesUtils.class.getClassLoader().getResourceAsStream("application.yaml");
+            props.load(in);
+
+            Iterator<Object> iterator = props.keySet().iterator();
+            while (iterator.hasNext()){
+                String key = (String) iterator.next();
+                PROPER_MAP.put(key, props.getProperty(key));
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if (in != null){
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static String getString(String key){
+        return props.getProperty(key);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getString("server.port"));
+    }
+
+ }
