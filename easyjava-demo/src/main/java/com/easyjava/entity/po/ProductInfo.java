b45b7708d46a447f60b2f20214e306d59486c39f
q2w2h2 <1349355096@qq.com>
2023-05-13 21:11:23 +0800

    生成PO 目前生成属性

--- easyjava-demo/src/main/java/com/easyjava/entity/po/ProductInfo.java
+++ easyjava-demo/src/main/java/com/easyjava/entity/po/ProductInfo.java
@@ -1,17 +1,65 @@
 package com.easyjava.entity.po;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
 
+
+/**
+ * @Description: 商品信息
+
+ * @author: 小阙
+ * @date: 2023/08/13
+ */
 public class ProductInfo implements Serializable {
+	/**
+	 * 自增id
+	 */
 	private Integer id;
+	/**
+	 * 公司id
+	 */
 	private String companyId;
+	/**
+	 * 商品编号
+	 */
 	private String code;
+	/**
+	 * 商品名称
+	 */
 	private String productName;
+	/**
+	 * 价格
+	 */
 	private BigDecimal price;
+	/**
+	 * sku模型
+	 */
 	private Integer skuType;
+	/**
+	 * 颜色类型
+	 */
 	private String colorType;
-	private Date creareTime;
-	private Date creareData;
+	/**
+	 * 创建时间
+	 */
+	@JsonFormat(pattern = "yyyy-mm-dd HH:mm:ss", timezone = "GMT+8")
+	@DateTimeFormat(pattern = "yyyy-mm-dd")
+	private Date createTime;
+	/**
+	 * 创建日期
+	 */
+	@JsonFormat(pattern = "yyyy-mm-dd", timezone = "GMT+8")
+	@DateTimeFormat(pattern = "yyyy-mm-dd")
+	private Date createData;
+	/**
+	 * 库存
+	 */
 	private Long stock;
+	/**
+	 * 状态·
+	 */
 	private Integer status;
 }
